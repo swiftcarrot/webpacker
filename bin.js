8d7c6453d2136c6b9fea975a0a2ead10594f1814
Wang Zuo <wzuoadjusted@gmail.com>
2017-09-13 23:54:59 +0800

    remote template

--- bin.js
+++ bin.js
@@ -3,20 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 const program = require('commander');
-const init = require('./src/init');
 const start = require('./src/start');
 const build = require('./src/build');
 const cwd = process.cwd();
 
 const configPath = path.resolve('cxx.config.js');
 const userConfig = fs.existsSync(configPath) ? require(configPath) : {};
-
-program
-  .command('init')
-  .description('init')
-  .action(function(options) {
-    init(userConfig);
-  });
 
 program
   .command('start')
