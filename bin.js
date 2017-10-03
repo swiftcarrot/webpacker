a3c49e74845732571d864501e53e44500a8348dd
Wang Zuo <wzuoadjusted@gmail.com>
2017-10-03 11:31:57 +0800

    fix examples

--- bin.js
+++ bin.js
@@ -3,22 +3,23 @@
 const fs = require('fs');
 const path = require('path');
 const program = require('commander');
-const start = require('./src/start');
+const watch = require('./src/watch');
 const build = require('./src/build');
 const clean = require('./src/clean');
+const devServer = require('./src/dev-server');
 const cwd = process.cwd();
 
 const configPath = path.resolve('cxx.config.js');
 const userConfig = Object.assign(
-  { clientOnly: true, webpack: { client: {}, server: {} } },
+  { clientOnly: true, webpack: { client: null, server: null } },
   fs.existsSync(configPath) ? require(configPath) : {}
 );
 
 program
-  .command('start')
-  .description('start')
+  .command('watch')
+  .description('watch')
   .action(function(options) {
-    start(userConfig);
+    watch(userConfig);
   });
 
 program
@@ -33,6 +34,13 @@ program
   .description('clean')
   .action(function(options) {
     clean(userConfig);
+  });
+
+program
+  .command('dev-server')
+  .description('dev server')
+  .action(function(options) {
+    devServer(userConfig);
   });
 
 program.parse(process.argv);
