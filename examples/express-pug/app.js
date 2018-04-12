4f0efec0ee33f06e48b15c298e3d6458b38e60a2
Wang Zuo <wzuoadjusted@gmail.com>
2018-04-12 12:15:08 +0800

    add manifest middleware

--- examples/express-pug/app.js
+++ examples/express-pug/app.js
@@ -1,36 +1,12 @@
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
-const env = process.env.NODE_ENV || 'development';
 const app = express();
 
 app.use('/packs', express.static(path.join(__dirname, 'build/packs')));
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-  if (env === 'production') {
-    const manifest = require('./build/packs/manifest.json');
-
-    res.locals.manifest = manifest;
-    res.locals.entrypointJS = e => manifest.entrypoints[e].js || [];
-    res.locals.entrypointCSS = e => manifest.entrypoints[e].css || [];
-
-    next();
-  } else {
-    const manifestPath = path.join(__dirname, 'build/packs/manifest.json');
-    fs.readFile(manifestPath, 'utf8', (err, data) => {
-      if (err) return next(err);
-      const manifest = JSON.parse(data);
-
-      res.locals.manifest = manifest;
-      res.locals.entrypointJS = e => manifest.entrypoints[e].js || [];
-      res.locals.entrypointCSS = e => manifest.entrypoints[e].css || [];
-
-      next();
-    });
-  }
-});
+app.use(require('../../manifest')(path.join(__dirname, 'build/packs')));
 
 app.get('/', (req, res) => {
   res.render('home');
---
+++ manifest.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const env = process.env.NODE_ENV || 'development';
+
+module.exports = packs => (req, res, next) => {
+  if (env === 'production') {
+    const manifest = require(path.join(packs, 'manifest.json'));
+
+    res.locals.manifest = manifest;
+    res.locals.entrypointJS = e => manifest.entrypoints[e].js || [];
+    res.locals.entrypointCSS = e => manifest.entrypoints[e].css || [];
+
+    next();
+  } else {
+    const manifestPath = path.join(packs, 'manifest.json');
+    fs.readFile(manifestPath, 'utf8', (err, data) => {
+      if (err) return next(err);
+      const manifest = JSON.parse(data);
+
+      res.locals.manifest = manifest;
+      res.locals.entrypointJS = e => manifest.entrypoints[e].js || [];
+      res.locals.entrypointCSS = e => manifest.entrypoints[e].css || [];
+
+      next();
+    });
+  }
+};
