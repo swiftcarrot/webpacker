77992d626167762b6bd79870817640a22e77a4c4
Wang Zuo <wzuoadjusted@gmail.com>
2018-10-16 21:53:44 +0800

    babel export-default-from

--- src/webpack/rules/babel.client.js
+++ src/webpack/rules/babel.client.js
@@ -17,7 +17,8 @@ module.exports = {
       require.resolve('@babel/plugin-transform-destructuring'),
       require.resolve('@babel/plugin-proposal-class-properties'),
       require.resolve('@babel/plugin-proposal-object-rest-spread'),
-      require.resolve('@babel/plugin-syntax-dynamic-import')
+      require.resolve('@babel/plugin-syntax-dynamic-import'),
+      require.resolve('@babel/plugin-proposal-export-default-from')
     ],
     cacheDirectory: true
   }
