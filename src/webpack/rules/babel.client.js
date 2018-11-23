1b0dd8e47b5d737cfaf0c1542187561a1dad5337
Wang Zuo <wangzuo@tiancai.pro>
2018-11-23 18:29:13 +0800

    add bable-plugin-emotion fix #23

--- src/webpack/rules/babel.client.js
+++ src/webpack/rules/babel.client.js
@@ -18,7 +18,8 @@ module.exports = {
       require.resolve('@babel/plugin-proposal-class-properties'),
       require.resolve('@babel/plugin-proposal-object-rest-spread'),
       require.resolve('@babel/plugin-syntax-dynamic-import'),
-      require.resolve('@babel/plugin-proposal-export-default-from')
+      require.resolve('@babel/plugin-proposal-export-default-from'),
+      require.resolve('babel-plugin-emotion')
     ],
     cacheDirectory: true
   }
