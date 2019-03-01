c872e34408f49bc22e131c92087da7edd18878e2
Wang Zuo <wzuoadjusted@gmail.com>
2019-03-01 19:38:32 +0800

    use babel preset

--- src/webpack/rules/babel.client.js
+++ src/webpack/rules/babel.client.js
@@ -3,24 +3,7 @@ module.exports = {
   exclude: /node_modules/,
   loader: require.resolve('babel-loader'),
   options: {
-    presets: [
-      [
-        require.resolve('@babel/preset-env'),
-        {
-          modules: false
-        }
-      ],
-      require.resolve('@babel/preset-flow'),
-      require.resolve('@babel/preset-react')
-    ],
-    plugins: [
-      require.resolve('@babel/plugin-transform-destructuring'),
-      require.resolve('@babel/plugin-proposal-class-properties'),
-      require.resolve('@babel/plugin-proposal-object-rest-spread'),
-      require.resolve('@babel/plugin-syntax-dynamic-import'),
-      require.resolve('@babel/plugin-proposal-export-default-from'),
-      require.resolve('babel-plugin-emotion')
-    ],
+    presets: [require.resolve('babel-preset-zkit')],
     cacheDirectory: true
   }
 };
