dec10ca3031267f1fb24c4a78b400771e79e4bfa
Wang Zuo <wzuoadjusted@gmail.com>
2019-04-03 20:57:48 +0800

    babel fix

--- src/webpack/rules/babel.client.js
+++ src/webpack/rules/babel.client.js
@@ -3,7 +3,7 @@ module.exports = {
   exclude: /node_modules/,
   loader: require.resolve('babel-loader'),
   options: {
-    presets: [require.resolve('../babel-preset-zkit')],
+    presets: [require.resolve('babel-preset-zkit')],
     cacheDirectory: true
   }
 };
