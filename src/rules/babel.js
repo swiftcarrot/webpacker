c8d328f100144f1c5ce8af0c51a10712a60ec08e
Wang Zuo <wzuoadjusted@gmail.com>
2019-05-14 15:46:46 +0800

    babel fix

--- src/rules/babel.js
+++ src/rules/babel.js
@@ -3,7 +3,7 @@ module.exports = {
   exclude: /node_modules/,
   loader: require.resolve('babel-loader'),
   options: {
-    presets: [require.resolve('@swiftcarrot/babel')],
+    presets: [require.resolve('babel-preset-swiftcarrot')],
     cacheDirectory: true
   }
 };
