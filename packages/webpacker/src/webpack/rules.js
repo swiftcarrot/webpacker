9dce70641e19b925f0189fe16ac5cbb9e54923ec
Wang Zuo <wzuoadjusted@gmail.com>
2020-08-26 15:15:07 +0800

    fix test

--- packages/webpacker/src/webpack/rules.js
+++ packages/webpacker/src/webpack/rules.js
@@ -39,7 +39,7 @@ module.exports = [
     exclude: /node_modules/,
     loader: require.resolve('babel-loader'),
     options: {
-      presets: [require.resolve('babel-preset')],
+      presets: [require.resolve('babel-preset-swiftcarrot')],
       cacheDirectory: true,
     },
   },
